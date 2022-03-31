d811743cdbd5aa7efbd07c980c4e6d0ffcd0dab1
Tristan Mastrodicasa <ghoststeam217@gmail.com>
2022-03-30 21:53:33 -0400

    Added TDD tools

---
+++ Assets/@Project/Tools/InputRecorderAutoPlay.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Unity.XR.Oculus;
+
+[RequireComponent(typeof(InputRecorder))]
+public class InputRecorderAutoPlay : MonoBehaviour
+{
+    InputRecorder recorder;
+    public string recorderTracePath;
+    public bool playOnAwake = false;
+
+	void Reset() {
+		recorder = GetComponent<InputRecorder>();
+		recorder.replayOnNewDevices = true;
+		recorder.simulateOriginalTimingOnReplay = true;
+		recorder.startRecordingWhenEnabled = false;
+	}
+
+    void Awake() {
+        if (playOnAwake) {
+            recorder = GetComponent<InputRecorder>();
+            recorder.LoadCaptureFromFile(recorderTracePath);
+            recorder.StartReplay();
+
+            StartCoroutine("SetUpDevices");
+        }
+    }
+
+    IEnumerator SetUpDevices() {
+        yield return new WaitForSeconds(0.2f);
+
+        var rightInputDevice = InputSystem.GetDevice("OculusTouchControllerRight");
+        InputSystem.SetDeviceUsage(rightInputDevice, "RightHand");
+
+        var leftInputDevice = InputSystem.GetDevice("OculusTouchControllerLeft");
+        InputSystem.SetDeviceUsage(leftInputDevice, "LeftHand");
+    }
+}
