e6dd324e9e838ca544c552788f082c7b0ffd89b7
Tristan Mastrodicasa <ghoststeam217@gmail.com>
2022-06-20 22:49:25 -0400

    Added nTools prefab painter

---
+++ Assets/nTools/PrefabPainter/Scripts/Editor/PrafabPainterGUI.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8dbbd3fa428f70694447b3f141f99f80d0653fec63841b77b78f6312517c1d34
+size 189862
---
+++ Assets/nTools/PrefabPainter/Scripts/Editor/PrefabPainter.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b8b76344f67b523cd150a02004c5aaec684f55285662a4272ffe4c6581aba1bb
+size 187632
