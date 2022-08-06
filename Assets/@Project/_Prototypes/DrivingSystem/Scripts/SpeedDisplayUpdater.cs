581ef412b633214277ade874fa6b9c4c4f1bb951
Tristan Mastrodicasa <ghoststeam217@gmail.com>
2022-08-06 15:22:30 -0400

    Added new dash monitor design

---
+++ Assets/@Project/_Prototypes/DrivingSystem/Scripts/SpeedDisplayUpdater.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace DuneRiders.DrivingSystem {
+	[RequireComponent(typeof(TextMeshProUGUI))]
+	public class SpeedDisplayUpdater : MonoBehaviour
+	{
+		[SerializeField] Rigidbody rb;
+		TextMeshProUGUI textMeshPro;
+
+		void Start()
+		{
+			textMeshPro = GetComponent<TextMeshProUGUI>();
+		}
+
+		void Update()
+		{
+			var kmH = Mathf.Abs(3.6f * rb.velocity.magnitude).ToString("0");
+			textMeshPro.SetText($"Speed\n{kmH} km/h");
+		}
+	}
+}
