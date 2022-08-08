0019b52d49bc669c241a657e11eefc1dbe91050a
Tristan Mastrodicasa <ghoststeam217@gmail.com>
2022-08-08 19:48:39 -0400

    Added crappy "show text on slider value" component

---
+++ Assets/@Project/_Prototypes/RiderTabletSystem/ShowOnSliderValue.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace DuneRiders.RiderTabletSystem {
+	[RequireComponent(typeof(TextMeshProUGUI))]
+	public class ShowOnSliderValue : MonoBehaviour
+	{
+		TextMeshProUGUI text;
+		string textShowing;
+		[SerializeField] Slider slider;
+		[SerializeField] float valueAtOrAboveToShow;
+
+		void Start() {
+			text = GetComponent<TextMeshProUGUI>();
+			textShowing = text.text;
+		}
+
+		void Update() {
+			if (slider.value >= 0.7f) {
+				text.text = textShowing;
+			} else {
+				text.text = "";
+			}
+		}
+	}
+}
