bf4d528717d41ca6f5518a806741ce2e26baf441
Tristan Mastrodicasa <ghoststeam217@gmail.com>
2022-08-09 23:49:53 -0400

    Added UI for thumbs up down interaction system

---
+++ Assets/@Project/_Prototypes/InteractionSystem/ThumbsUpDownInteraction/ThumbsUpDownDisplay.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DuneRiders.InteractionSystem.ThumbsUpDownInteraction {
+	public class ThumbsUpDownDisplay : MonoBehaviour
+	{
+		[SerializeField] private Canvas decisionProgressCanvas;
+		[SerializeField] private Image decisionProgressImage;
+		[SerializeField] private ThumbsUpDownResponseInterface thumbsUpDownResponseInterface;
+
+
+		void Update() {
+			if (thumbsUpDownResponseInterface.Active) {
+				decisionProgressCanvas.enabled = true;
+				decisionProgressImage.fillAmount = thumbsUpDownResponseInterface.ConfirmationProgress;
+			} else {
+				decisionProgressCanvas.enabled = false;
+			}
+		}
+	}
+}
