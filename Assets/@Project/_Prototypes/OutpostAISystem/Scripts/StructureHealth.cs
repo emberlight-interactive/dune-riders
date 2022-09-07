fb9a8d52f87009878edc3b917460a055bc32976f
Tristan Mastrodicasa <ghoststeam217@gmail.com>
2022-09-07 11:22:40 -0400

    Added global state to outpost structure health

--- Assets/@Project/_Prototypes/OutpostAISystem/Scripts/StructureHealth.cs
+++ Assets/@Project/_Prototypes/OutpostAISystem/Scripts/StructureHealth.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,23 @@ using UnityEngine;
 namespace DuneRiders.OutpostAI {
 	public class StructureHealth : MonoBehaviour
 	{
-		public int health = 50;
+		[Serializable]
+		class StructureHealthState {
+			public int health;
+		}
+
+		StructureHealthState state;
+		ProceduralTools proceduralTools;
+		public int health { get => state.health; set => state.health = value; }
+
+		void Awake() {
+			proceduralTools = new ProceduralTools(transform);
+			GlobalState.InitState<StructureHealthGlobalState, string, StructureHealthState>(
+				proceduralTools.BuildTransformHash(),
+				new StructureHealthState() { health = 50 },
+				out state
+			);
+		}
 
 		void FixedUpdate() {
 			// todo: Do the turrets still register after destruction ??
@@ -13,5 +30,7 @@ namespace DuneRiders.OutpostAI {
 			// and add that to the behaviour tree
 			if (health <= 0) gameObject.SetActive(false);
 		}
+
+		class StructureHealthGlobalState : GlobalStateGameObject<string, StructureHealthState> {}
 	}
 }
