dd38a2bacd209bbe721b6262a81cfed90d8137be
Tristan Mastrodicasa <ghoststeam217@gmail.com>
2022-09-27 16:43:09 -0400

    Added village persistence

---
+++ Assets/Easy Save 3/Types/ES3UserType_HomeVillageFuelManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8ec0ac32bf5d23a0756eb0b63853fbf94c933e0ea07ebdcd6177222baf67b382
+size 1677
---
+++ Assets/Easy Save 3/Types/ES3UserType_VillageMigrationManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e5997f46f526fc122a754424dccd68bfbd055503ffaf35fdc97fcca441c5689f
+size 1864
