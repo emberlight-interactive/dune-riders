6116ad65baa1ba85f9e840e0815e4abb62e9b880
Tristan Mastrodicasa <ghoststeam217@gmail.com>
2022-10-10 17:31:22 -0400

    Added command pulsing to allow multiple halts

--- Assets/@Project/_Prototypes/_Combinations/PlayerRiderControllerCombination/SliderUpdatesRiderAICommand.cs
+++ Assets/@Project/_Prototypes/_Combinations/PlayerRiderControllerCombination/SliderUpdatesRiderAICommand.cs
@@ -9,14 +9,24 @@ namespace DuneRiders.PlayerRiderControllerCombination {
 		PlayerCommandState.PlayerCommandGlobalState globalCommandState;
 		[SerializeField] PlayerCommandState.Command commandToSet;
 
+		[SerializeField, Header("Pulse Command (Run a command before the intended one)")] bool pulseCommand = false;
+		[SerializeField] PlayerCommandState.Command commandToPulse;
+
 		public void UpdateRiderAICommand() {
 			if (globalCommandState == null) LinkToGlobalState();
 
-			globalCommandState.command = commandToSet;
+			if (pulseCommand) StartCoroutine(PulseCommand());
+			else globalCommandState.command = commandToSet;
 		}
 
 		void LinkToGlobalState() {
 			globalCommandState = FindObjectOfType<PlayerCommandState.PlayerCommandGlobalState>();
+		}
+
+		IEnumerator PulseCommand() {
+			globalCommandState.command = commandToPulse;
+			yield return null;
+			globalCommandState.command = commandToSet;
 		}
 	}
 }
