2b7517c51c05c9500619941e2ce6491d9376b871
Tristan Mastrodicasa <ghoststeam217@gmail.com>
2022-11-11 22:13:20 -0500

    Persist wheel command

--- Assets/@Project/_Prototypes/CommandHonkSystem/CommandSelector.cs
+++ Assets/@Project/_Prototypes/CommandHonkSystem/CommandSelector.cs
@@ -4,16 +4,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
+using DuneRiders.Shared.PersistenceSystem;
 
 namespace DuneRiders.CommandHonkSystem {
-	public class CommandSelector : MonoBehaviour
+	public class CommandSelector : MonoBehaviour, IPersistent
 	{
+		[Serializable]
+		class CommandSelectorSerializable {
+			public int currentCommandIndex;
+		}
+
 		[SerializeField] TextMeshProUGUI currentlySelectedCommandDisplay;
 		[SerializeField] InputActionProperty selectorToggle;
 		[SerializeField] List<Command> commands = new List<Command>();
 		int currentCommandIndex = 0;
 		bool selected = false;
 		public Command CurrentCommand { get; private set;}
+		public bool DisablePersistence { get => false; }
+		string persistenceKey = "CommandSelectorDisplay";
 
 		void Start() {
 			selectorToggle.action.Enable();
@@ -66,6 +74,19 @@ namespace DuneRiders.CommandHonkSystem {
 
 		void UpdateDisplay() {
 			currentlySelectedCommandDisplay.text = CurrentCommand.commandName;
+		}
+
+		public void Save(IPersistenceUtil persistUtil) {
+			persistUtil.Save(persistenceKey, new CommandSelectorSerializable {
+				currentCommandIndex = this.currentCommandIndex,
+			});
+		}
+
+        public void Load(IPersistenceUtil persistUtil) {
+			var loadedCommandSelector = persistUtil.Load<CommandSelectorSerializable>(persistenceKey);
+			this.currentCommandIndex = loadedCommandSelector.currentCommandIndex;
+			CurrentCommand = commands[currentCommandIndex];
+			UpdateDisplay();
 		}
 	}
 }
