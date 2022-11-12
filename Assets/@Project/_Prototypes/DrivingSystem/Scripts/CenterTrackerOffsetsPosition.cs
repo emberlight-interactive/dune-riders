6a52044146764f535bf25505a131eb8abb86bce7
Tristan Mastrodicasa <ghoststeam217@gmail.com>
2022-11-11 19:41:42 -0500

    Added height adjuster at start of scenes

--- Assets/@Project/_Prototypes/DrivingSystem/Scripts/CenterTrackerOffsetsPosition.cs
+++ Assets/@Project/_Prototypes/DrivingSystem/Scripts/CenterTrackerOffsetsPosition.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class CenterTrackerOffsetsPosition : MonoBehaviour
 {
+	[SerializeField] bool setHeightWhenBelowThreshold = false;
+	[SerializeField] Transform cam;
+	[SerializeField] Transform camOffset;
+	[SerializeField] Transform camFollowerOffset;
+
+	[SerializeField] float heightMinimumThreshold;
+	[SerializeField] float heightToSet;
+
 	void Start()
     {
         StartCoroutine(Center());
@@ -14,6 +22,14 @@ public class CenterTrackerOffsetsPosition : MonoBehaviour
 		while (transform.localPosition != Vector3.zero) {
 			transform.localPosition = Vector3.zero;
 			yield return new WaitForSeconds(0.1f);
+		}
+
+		if (setHeightWhenBelowThreshold) {
+			if (cam.localPosition.y <= heightMinimumThreshold) {
+				var camYOffset = heightToSet - cam.localPosition.y;
+				camOffset.localPosition = new Vector3(camOffset.localPosition.x, camYOffset, camOffset.localPosition.z);
+				camFollowerOffset.localPosition = new Vector3(camFollowerOffset.localPosition.x, camYOffset, camFollowerOffset.localPosition.z);
+			}
 		}
 	}
 }
