1a0b99c8375a0872fafca697ad86f894fb3a48dc
Tristan Mastrodicasa <ghoststeam217@gmail.com>
2022-11-17 01:31:15 -0500

    Added warning color when low fuel

--- Assets/@Project/_Prototypes/_Combinations/PlayerRiderControllerCombination/FuelDisplayUpdater.cs
+++ Assets/@Project/_Prototypes/_Combinations/PlayerRiderControllerCombination/FuelDisplayUpdater.cs
@@ -17,7 +17,16 @@ namespace DuneRiders.PlayerRiderControllerCombination {
 			fuelText = GetComponent<TextMeshProUGUI>();
 		}
 
-		void Update() {
+		void Start() {
+			InvokeRepeating(nameof(UpdateText), 0f, 1f);
+		}
+
+		void UpdateText() {
+			var playerFuelManager = gatherer.GetManager(Gatherer.SupportedResources.Fuel);
+
+			if (playerFuelManager.Amount() / playerFuelManager.ResourceLimit() < 0.07f) fuelText.color = Color.red;
+			else fuelText.color = Color.white;
+
 			fuelText.text = $"{(int) gatherer.GetManager(Gatherer.SupportedResources.Fuel).Amount()} L";
 			fuelBurnRateText.text = $"-{burnRateSystem.GetResourceBurnRate(BurnRateSystem.ResourceType.Fuel)} L/h";
 		}
