22f9dceaec8a2957fe272ae4ad86e4c056deb661
Tristan Mastrodicasa <ghoststeam217@gmail.com>
2022-11-22 13:36:27 -0500

    Hide map icon when too far away

--- Assets/@Project/_Prototypes/RiderTabletSystem/MapIcon.cs
+++ Assets/@Project/_Prototypes/RiderTabletSystem/MapIcon.cs
@@ -6,18 +6,44 @@ namespace DuneRiders.RiderTabletSystem {
 	public class MapIcon : bl_MiniMapEntity
 	{
 		[SerializeField] bool hideIconWhenGameObjectIsDisabled = true;
+		[SerializeField] float maxDistanceDetectable = 2000f;
+		bl_MiniMap minimap;
 
 		void Awake() {
-			var minimap = GameObject.FindObjectOfType<bl_MiniMap>(true);
+			minimap = GameObject.FindObjectOfType<bl_MiniMap>(true);
 			if (!minimap.gameObject.activeSelf) enabled = false;
 		}
 
 		void OnEnable() {
-			if (hideIconWhenGameObjectIsDisabled) SetActiveIcon(true);
+			if (hideIconWhenGameObjectIsDisabled) SetIconActiveIdempotent(true);
+			StartCoroutine(HandleMapIconOutOfBounds());
 		}
 
 		void OnDisable() {
-			if (hideIconWhenGameObjectIsDisabled) SetActiveIcon(false);
+			StopAllCoroutines();
+			if (hideIconWhenGameObjectIsDisabled) SetIconActiveIdempotent(false);
+		}
+
+		IEnumerator HandleMapIconOutOfBounds() {
+			while (true) {
+				yield return new WaitForSeconds(2f);
+
+				if (IconOutOfBounds()) {
+					SetIconActiveIdempotent(false);
+				} else {
+					SetIconActiveIdempotent(true);
+				}
+			}
+		}
+
+		void SetIconActiveIdempotent(bool setActive) {
+			if (!setActive || (setActive && !IconOutOfBounds())) {
+				SetActiveIcon(setActive);
+			}
+		}
+
+		bool IconOutOfBounds() {
+			return Vector3.Distance(minimap.transform.position, transform.position) > maxDistanceDetectable;
 		}
 	}
 }
